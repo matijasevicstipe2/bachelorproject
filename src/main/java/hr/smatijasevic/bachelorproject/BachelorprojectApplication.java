391e3797e14102c78a7c3d51498d44373eb84b8e
matijasevicstipe2 <smatijasevic57@gmail.com>
2023-05-14 01:01:29 +0200

    Initial commit

---
+++ src/main/java/hr/smatijasevic/bachelorproject/BachelorprojectApplication.java
@@ -0,0 +1,13 @@
+package hr.smatijasevic.bachelorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BachelorprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/smatijasevic/bachelorproject/BachelorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.smatijasevic.bachelorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
