1c130dc000c7e91495b154e879296aff72b3f465
matijasevicstipe2 <smatijasevic57@gmail.com>
2023-09-28 16:52:28 +0200

    27 commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import {FullCalendarModule} from "@fullcalendar/angular";
 import {EventPopupComponent} from './event-popup/event-popup.component';
 import {CardComponent} from "./card/card.component";
 import { ExampleNameComponent } from './example-name/example-name.component';
+import { CcComponent } from './cc/cc.component';
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import { ExampleNameComponent } from './example-name/example-name.component';
     HomeComponent,
     EventPopupComponent,
     CardComponent,
-    ExampleNameComponent
+    ExampleNameComponent,
+    CcComponent
   ],
   imports: [
     CommonModule,
